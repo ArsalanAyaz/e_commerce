49084f4b839e16356d845776510ffc5e7c546289
ArsalanAyaz <henrykoff7788@gmail.com>
2024-02-14 13:00:58 +0500

    slide1

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,10 @@ import { HeaderTop } from "@/components/HeaderTop";
 import { HeaderMain } from "@/components/HeaderMain";
 import Navbar from "@/components/Navbar";
 
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -24,6 +28,7 @@ export default function RootLayout({
         <HeaderTop/>
         <HeaderMain/>
         <Navbar/>
+        
         {children}
         
         </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
+import Hero from "@/components/Hero";
+
+
 export default function Home() {
   return (
-    <h1></h1>
+    
+
+    <Hero/>
+
+    
   );
 }
